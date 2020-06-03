bd00c6183de8c3ac8c740c802399520f10c04fdd
Jason7Lima <54913875+Jason7Lima@users.noreply.github.com>
2020-06-03 18:02:34 -0400

    Completed Task 11-18 and 20

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ class Alphabet(list):
 	def __init__(self, val=[]):
 		super(Alphabet, self).__init__(val)
 
+	def is_empty(self):
+		return not len(self)
+
 	def __repr__(self):
 		return 'Alphabet {}'.format(', '.join(map(str, self))) 
 
@@ -14,11 +17,13 @@ class Alphabet(list):
 #This is our own string data type which will has an empty func in there and returns the string in a readable format
 class String(list):
 	def __init__(self, st, al):
+		if isinstance(st,str):
+			st = [Char(c) for c in list(st)]
 		super(String, self).__init__(st)
 		self.al = al
 
 	def empty(self):
-		return not len(self.st)
+		return not len(self)
 
 	def __repr__(self):
 		return 'String {}'.format(''.join(map(str,self)))
@@ -29,18 +34,25 @@ class Char():
 	def __init__(self, ch=''):
 		self.ch = ch;
 
+	def __hash__(self):
+		return hash(self.ch)
+
 	def __eq__(self, other):
 		return self.ch == other
 
+	def __repr__(self):
+		return self.ch
+
 
 def nthString(alp, num):
+	if not num: return String([Char()], alp)
 	layer = int(math.log(num + 1, len(alp))) 
 	index =  num + 1 - len(alp) ** layer 
 	
 	return String(list(product(alp, repeat=layer))[index], alp)
 
 class DFA():
-	def __init__(self, Q, alpha, iniQ, trans, F):
+	def __init__(self, alpha, Q, iniQ, trans, F):
 		self.Q = Q
 		self.alpha = alpha
 		self.iniQ = iniQ
@@ -48,31 +60,153 @@ class DFA():
 		self.F = F
 
 	def accepts(self, w):
-		qi = self.iniQ
-		i = 0
-		for i in range(len(w)):
-			qi = self.trans(qi, w[i])
 
-		return self.F(qi) 
+		if self.Q:
+			if not w:
+				return False
+			elif w == [Char()]:
+				return True
+		else:
+			if w.is_empty():
+				return True
+			return False
+
+		qi = self.iniQ
+		for i in w:
+			qi = self.trans[qi][i]
+
+		return qi in self.F 
+
+	def get_accept(self):
+		v = set()
+		a = []
+
+		def accept(qi):
+			if qi in self.F:
+				return True
+			elif qi in v:
+				return False
+			v.add(qi)
+
+			for c in self.alpha:
+				if accept(self.trans[qi][c]):
+					a.append(c)
+					return True
+			return False
+
+		accept(self.iniQ)
+
+		return String(a[::-1], self.alpha)
+
+	def trace(self, s):
+		state = []
+		if self.accepts(s):
+			qi = self.iniQ
+			for c in s:
+				state.append(qi)
+				qi = self.trans[qi][c]
+			return state
+		return False
+
+	def cross(self, other, cond):
+		states = set()
+		accepts = set()
+		delta = dict()
+
+		for qi1 in self.Q:
+			for qi2 in other.Q:
+				states.add((qi1, qi2))
+				delta[(qi1, qi2)] = dict()
+				for c in self.alpha:
+					delta[(qi1, qi2)][c] = (self.trans[qi1][c], other.trans[qi2][c])
+
+		for (qi1, qi2) in states:
+			if cond(qi1 in self.F, qi2 in other.F):
+				accepts.add((qi1, qi2))
+
+		return DFA(self.alpha, states, (self.iniQ, other.iniQ), delta, accepts)
+
+	def union(self, other):
+		return self.cross(other, bool.__or__)
+
+	def intersect(self, other):
+		return self.cross(other, bool.__and__)
+
+	def subset(self, other):
+		'''
+			C := B intersect A^c
+			A is a subset of B iff C does not have any acceptable strings
+		'''
+		return not other.intersect(~self).get_accepted()
+
+	def __contains__(self, other):
+		return self.subset(other)
+
+	def __eq__(self, other):
+		'''
+			if both sides are a subset of each other then they are equal
+		'''
+		return self in other and other in self
+
+	def __invert__(self):
+		'''
+			returns the complement of the DFA with a new name
+		'''
+		return DFA(self.alpha, self.Q, self.iniQ, self.trans, self.Q - self.F)
+
+
+	
+	def compliment(d):
+		return DFA(d.alpha, d.Q, d.iniQ, d.trans, d.Q - d.F)
+
+
+
+
+
+
+
+
 
 
 binary = Alphabet([Char('0'), Char('1')])
 alpha = Alphabet([Char(letters) for letters in 'abcdefghijklmnopqrstuvwxyz'])
 
-dfaNoString = DFA(binary,
-					 (lambda qi: False), 0,
-					 (lambda qi, c: False),
-					 (lambda qi: False)) 
 
-dfaEmpty = DFA(binary,
-					   (lambda qi: qi == 0 or qi == 1), 0,
-					   (lambda qi, c: 1 if c else 0),
-					   (lambda qi: qi == 0)) 
+contains_001 = DFA(binary,
+					   {'iniQ', 'q1', 'q2', 'q3'}, 'iniQ',
+					   {
+						   	'iniQ': {Char('0'): 'q1', Char('1'): 'iniQ'},
+						    'q1': {Char('0'): 'q2', Char('1'): 'iniQ'},
+						    'q2': {Char('1'): 'q3', Char('0'): 'q2'},
+						    'q3': {Char('0'): 'q3', Char('1'): 'q3'}
+					   },
+					   {'q3'})
 
-dfaSingleChar = DFA(binary,
-						(lambda qi: qi == 0 or qi == 1 or qi == 2), 0,
-						(lambda qi, c: 1 if qi == 0 and c else 2),
-						(lambda qi: qi == 1)) 
+only_ones = DFA(binary,
+					{'q0', 'q1', 'q2'}, 'q0',
+					{
+						'q0': {Char('0'): 'q2', Char('1'): 'q1'},
+						'q1': {Char('0'): 'q2', Char('1'): 'q1'},
+						'q2': {Char('0'): 'q2', Char('1'): 'q2'}
+					},
+					{'q1'})
+
+RepetitiveOnes = DFA(binary,
+						   {'q0', 'q1', 'q2'}, 'q0',
+						   {
+						   	'q0': {Char('1'): 'q1', Char('0'): 'q0'},
+						    'q1': {Char('1'): 'q2', Char('0'): 'q0'},
+						    'q2': {Char('1'): 'q2', Char('0'): 'q2'},
+						   },
+						   {'q2'})
+
+even_length = DFA(binary,
+					  {'q0', 'q1'}, 'q0',
+					  {
+						'q0': {Char('0'): 'q1', Char('1'): 'q1'},
+						'q1': {Char('0'): 'q0', Char('1'): 'q0'}
+					  },
+					  {'q0'})
 
 def dfaFailCase(d, s, expected):
 	if d.accepts(s) != expected:
@@ -88,63 +222,33 @@ def dfaTestingDriver(d, tests):
 	print(f'{passed}/{len(tests)} tests PASSED')
 	return passed
 
-#contains 0
-Zeros = DFA(binary,
-	(lambda qi: qi == 0 or qi == 1 or qi == 2), 0,
-	(lambda qi, c: 1 if qi == 0 and c == '0' else 2 if qi == 1 and c == '0' else 2 if qi == 2 else 0),
-	(lambda qi: qi == 2))
-#contains one
-Ones = DFA(binary,
-	(lambda qi: qi == 0 or qi == 1 or qi == 2), 0,
-	(lambda qi, c: 1 if qi == 0 and c == '1' else 2 if qi == 1 and c == '1' else 2 if qi == 2 else 0),
-	(lambda qi: qi == 2))
-
-#even amount of elements
-dfaEven = DFA(binary,
-	(lambda qi: qi == 0 or qi == 1), 0,
-	(lambda qi, c: 1 if qi == 0 and c else 0),
-	(lambda qi: qi == 0))
-#odd amount of element
-dfaOdd = DFA(binary,
-    (lambda qi: qi == 0 or qi == 1), 0,
-    (lambda qi, c: 1 if qi == 0 and c else 0),
-    (lambda qi: qi == 1))
-#bin value even
-dfaBinEven = DFA(binary,
-    (lambda qi: qi == 0 or qi == 1), 0,
-    (lambda qi, c: 1 if c == '0' else 0),
-    (lambda qi: qi == 1))
-#bin value is odd
-dfaBinOdd = DFA(binary,
-   (lambda qi: qi == 0 or qi == 1), 0,
-   (lambda qi, c: 0 if c == '0' else 1),
-   (lambda qi: qi == 1))
-
-#random test cast to see if the first element is a *
-dfaStrCase = DFA(alpha,
-	(lambda qi: qi == 0 or qi == 1 or qi == 2), 0,
-	(lambda qi, c: 1 if qi == 0 and c == '*' else 1 if qi == 1 else 2),
-	(lambda qi: qi == 1))
+##########################################################################################################################################################################################################
 
 
+										#Testing for Intersect task 16
 
-tests = [('0', False), ('1', False), ('0000', True), ('0100', True), ('010010', True)]
-dfaTestingDriver(Zeros, tests)
 
-tests = [('0', False), ('1', False), ('1111', True), ('1011', True), ('101101', True)]
-dfaTestingDriver(Ones, tests)
+RepetitiveOnes_and_contains_001 = RepetitiveOnes.intersect(contains_001)
+even_length_and_only_ones = even_length.intersect(only_ones)
 
-tests = [('0', False), ('1', False), ('000', True), ('011', False), ('0000', True)]
-dfaTestingDriver(dfaEven, tests)
+# Test DFA that accepts strings accepted by either RepetitiveOnes and contains_001
+test_cases = [([], False), ('0', False), ('01', False), ('11', False), ('111', False), ('110', False), ('0011', True), ('10011', True), ('00011', True), ('11001', True), ('111001', True), ('1001111', True)]
+dfaTestingDriver(RepetitiveOnes_and_contains_001, test_cases)
 
-tests = [('0', True), ('1', True), ('010', True), ('011', True), ('0000', False)]
-dfaTestingDriver(dfaOdd, tests)
 
-tests = [('0', True), ('1', False), ('010', True), ('011', False), ('1110', True)]
-dfaTestingDriver(dfaBinEven, tests)
+##########################################################################################################################################################################################################
 
-tests = [('0', False), ('1', True), ('010', False), ('011', True), ('1011', True)]
-dfaTestingDriver(dfaBinOdd, tests)
+##########################################################################################################################################################################################################
 
-tests = [('*', True), ('*123', True), ('123*', False), ('trade***', False), ('', False)]
-dfaTestingDriver(dfaStrCase, tests)
+
+										#Testing for Union task 15
+
+
+RepetitiveOnes_or_contains_001 = RepetitiveOnes.union(contains_001)
+EvenLength_OnlyOnes = even_length.union(only_ones)
+
+# Test DFA that accepts strings accepted by either RepetitiveOnes or contains_001
+test_cases = [([], False), ('0', False), ('01', False), ('11', True), ('111', True), ('110', True), ('010', False), ('0011', True), ('0001', True), ('00011', True), ('0000', False), ('01000', False)]
+dfaTestingDriver(RepetitiveOnes_or_contains_001, test_cases)
+
+##########################################################################################################################################################################################################
